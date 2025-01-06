3dbdf93d0822c819a0934c6b1df2effc7f21d511
mjnhd <mjnhdi35@gmail.com>
2025-01-06 19:52:14 +0700

    fix(user,auth)

--- apps/api/src/services/auth.service.ts
+++ apps/api/src/services/auth.service.ts
@@ -3,21 +3,32 @@ import { Injectable } from '../core/decorators/injectable.decorator'
 import { Inject } from '../core/decorators/param.decorator'
 import { User } from '../db/models/user.model'
 import jsonwebtoken from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
   constructor(@Inject(User) private userModel: typeof User) {}
 
   async login(body: any) {
+    const { email, password } = body
     const user = await this.userModel
       .findOne({
         email: body.email,
       })
       .lean()
-    if (!user || user.password !== body.password) {
+
+    if (!user) {
       throw new BadRequestException('Email hoặc password không đúng!')
     }
-    const { password, ...payload } = user
+    // console.log('Stored hash password:', user.password)
+    // console.log('Input password:', password)
+    const isPasswordValid = await bcrypt.compare(password, user.password!)
+
+    if (!isPasswordValid) {
+      throw new BadRequestException('Email hoặc password không đúng!')
+    }
+
+    const { password: userPassword, ...payload } = user
     const access_token = jsonwebtoken.sign(
       payload,
       process.env.JWT_SECRET_KEY!,
--- apps/api/src/services/user.service.ts
+++ apps/api/src/services/user.service.ts
@@ -2,12 +2,28 @@ import { BadRequestException } from '../core/base/error.base'
 import { Injectable } from '../core/decorators/injectable.decorator'
 import { Inject } from '../core/decorators/param.decorator'
 import { User } from '../db/models/user.model'
-
+import bcrypt from 'bcrypt'
 @Injectable()
 export class UserService {
   constructor(@Inject(User) private userModel: typeof User) {}
   async create(body: any) {
-    return await this.userModel.create(body)
+    const { password } = body
+    if (!password) {
+      throw new BadRequestException('full field is required')
+    }
+    // console.log('Password before hashing:', password)
+
+    const rounds = process.env.ROUND ? parseInt(process.env.ROUND, 10) : 10
+    if (isNaN(rounds) || rounds < 1) {
+      throw new BadRequestException('Invalid salt rounds')
+    }
+
+    const hashedPassword = await bcrypt.hash(password, rounds)
+    // console.log('Hashed password:', hashedPassword)
+    return await this.userModel.create({
+      ...body,
+      password: hashedPassword,
+    })
   }
   async find() {
     return await this.userModel.find()
