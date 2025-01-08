d7aa6fd30473472fbcb68dfcb0f1bc9e58b386b6
mjnhd <mjnhdi35@gmail.com>
2025-01-08 07:59:16 +0700

    test: call api users

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -91,10 +91,10 @@ async function bootstrap() {
   app.use(
     '*',
     cors({
-      origin: 'http://localhost:5173', // Your frontend URL
+      origin: 'http://localhost:5173',
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true, // Allow cookies
+      credentials: true,
     }),
   )
 
